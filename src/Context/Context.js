f9956886d7139d4a38d5e3c56341a251ec7f49cf
nikaKh12 <97915032+nikaKh12@users.noreply.github.com>
2023-02-03 21:02:25 +0400

    Set up Context

---
+++ src/Context/Context.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+const Context = React.createContext();
+
+function ContextProvider({ children }) {
+  const [validateNameSuccess, setValidateNameSuccess] = useState("");
+  const [validateMailSuccess, setValidateMailSuccess] = useState("");
+  const [validatePhoneSuccess, setValidatePhoneSuccess] = useState("");
+
+  const validateName = (event) => {
+    let value = event.target.value;
+    let regex = /^[\u10A0-\u10FF]+$/;
+    if (value.length >= 2 && regex.test(value)) {
+      setValidateNameSuccess(true);
+      event.target.style.border = "1px solid green";
+    } else {
+      setValidateNameSuccess(false);
+      event.target.style.border = "1px solid red";
+    }
+  };
+
+  const validateMail = (event) => {
+    let value = event.target.value;
+    let check = "@redberry.ge";
+    if (check === value.slice(-12)) {
+      setValidateMailSuccess(true);
+      event.target.style.border = "1px solid green";
+    } else {
+      setValidateMailSuccess(false);
+      event.target.style.border = "1px solid red";
+    }
+  };
+
+  const validateNumber = (event) => {
+    let value = event.target.value;
+    let regex = /^\+[0-9]{12}$/;
+    if (regex.test(value)) {
+      setValidatePhoneSuccess(true);
+      event.target.style.border = "1px solid green";
+    } else {
+      setValidatePhoneSuccess(false);
+      event.target.style.border = "1px solid red";
+    }
+  };
+
+  return (
+    <Context.Provider
+      value={{
+        validateNameSuccess,
+        setValidateNameSuccess,
+        validateMailSuccess,
+        setValidateMailSuccess,
+        validatePhoneSuccess,
+        setValidatePhoneSuccess,
+        validateName,
+        validateMail,
+        validateNumber,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+}
+
+export { ContextProvider, Context };
