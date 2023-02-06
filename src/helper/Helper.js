27cc00a0cd1968b8b8d134edb24c4e77fc4d0e9b
nikaKh12 <97915032+nikaKh12@users.noreply.github.com>
2023-02-06 17:20:57 +0400

    Added helper functions

--- src/helper/Helper.js
+++ src/helper/Helper.js
@@ -1,4 +1,29 @@
 const regex = /^[\u10A0-\u10FF]+$/;
 const phoneRegex = /^\+995\d{3}\d{2}\d{2}\d{2}|^\+995 \d{3} \d{2} \d{2} \d{2}$/;
 
-export { regex, phoneRegex };
+const toggleValidation = (ref, check) => {
+  if (check) {
+    ref.current.style.visibility = "hidden";
+  }
+};
+
+const changeColor = (ref, color) => {
+  ref.current.style.color = color;
+};
+
+const setBorder = (ref, value) => {
+  ref.current.style.border = value;
+};
+
+const setVisibility = (ref, value) => {
+  ref.current.style.visibility = value;
+};
+
+export {
+  regex,
+  phoneRegex,
+  toggleValidation,
+  changeColor,
+  setBorder,
+  setVisibility,
+};
