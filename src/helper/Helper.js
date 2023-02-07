8bbb6df502e3821d685051ab3d9dc0187d98ce49
nikaKh12 <97915032+nikaKh12@users.noreply.github.com>
2023-02-07 20:34:49 +0400

    Fixed phone regex

--- src/helper/Helper.js
+++ src/helper/Helper.js
@@ -1,5 +1,6 @@
 const regex = /^[\u10A0-\u10FF]+$/;
-const phoneRegex = /^\+995\d{3}\d{2}\d{2}\d{2}|^\+995 \d{3} \d{2} \d{2} \d{2}$/;
+const phoneRegex =
+  /^\+995\d{3}\d{2}\d{2}\d{2}$|^\+995 \d{3} \d{2} \d{2} \d{2}$|^\+995 \d{3}\d{2}\d{2}\d{2}$/;
 
 const toggleValidation = (ref, check) => {
   if (check) {
