aa2095624bd6b07e5c4e1159c16c119c23ed24f3
nikaKh12 <97915032+nikaKh12@users.noreply.github.com>
2023-02-12 02:26:13 +0400

    Added education route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import HomePage from "./pages/HomePage";
 import PrivateInfo from "./pages/PrivateInfo";
 import Experience from "./pages/Experience";
+import Education from "./pages/Education";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" element={<HomePage />}></Route>
         <Route path="/private-info" element={<PrivateInfo />}></Route>
         <Route path="/experience" element={<Experience />}></Route>
+        <Route path="/education" element={<Education />}></Route>
       </Routes>
     </div>
   );
